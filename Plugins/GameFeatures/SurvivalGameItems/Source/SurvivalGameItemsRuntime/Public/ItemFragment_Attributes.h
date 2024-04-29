80068e1e6822b4349c8f1b31dfb23e76e87bd812
Angelo Barandino <angelobarandino24@gmail.com>
2024-04-29 13:21:05 +0800

    crafting and attribute fragment types

---
+++ Plugins/GameFeatures/SurvivalGameItems/Source/SurvivalGameItemsRuntime/Public/ItemFragment_Attributes.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemFragment.h"
+#include "ItemFragment_Attributes.generated.h"
+
+UCLASS(meta=(DisplayName = "Attributes"))
+class UItemFragment_Attributes : public UItemFragment
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes", meta=(Tooltip="Strength of the item's effect. (e.g., Healing amount, Damage dealt, Buff magnitude)"))
+	float EffectMagnitude = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes", meta=(MultiLine=true, Tooltip="Text description of the item's effect. (e.g., Heals X health points, Increases Damage by Y%, Grants Z buff)"))
+	FText EffectDescription;
+ 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes", meta=(Tooltip="Numeric value for remaining durability (if applicable)"))
+	float Durability = 0.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes", meta=(Tooltip="Measures how long the item can be used before needing to recharge. (Applicable to consumables or abilities)"))
+	float Cooldown = 0.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes", meta=(Tooltip="Minimum character level required to use this item effectively."))
+	float LevelRequirement = 0.f;
+	
+};
---
+++ Plugins/GameFeatures/SurvivalGameItems/Source/SurvivalGameItemsRuntime/Public/ItemFragment_Crafting.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemFragment.h"
+#include "ItemFragment_Crafting.generated.h"
+
+class UItemDefinition;
+
+USTRUCT(BlueprintType)
+struct FCraftIngredient
+{
+	GENERATED_BODY()
+
+	/*
+	 * 
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Crafting Material")
+	TSoftClassPtr<UItemDefinition> Item = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Crafting Material")
+	int32 RequiredCount = 1;
+};
+
+UCLASS(meta=(DisplayName = "Crafting"))
+class UItemFragment_Crafting : public UItemFragment
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Crafting")
+	TArray<FCraftIngredient> Ingredients;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Crafting")
+	int32 ProducedItems = 1;
+	
+};
