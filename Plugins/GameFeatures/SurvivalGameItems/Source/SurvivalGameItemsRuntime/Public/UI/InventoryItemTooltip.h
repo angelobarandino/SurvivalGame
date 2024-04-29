60841a16b855ea9adb605ef2809382064f0f70fd
Angelo Barandino <angelobarandino24@gmail.com>
2024-04-29 13:21:41 +0800

    base item widget tooltip class

---
+++ Plugins/GameFeatures/SurvivalGameItems/Source/SurvivalGameItemsRuntime/Public/UI/InventoryItemTooltip.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "InventorySystem/InventoryItemInstance.h"
+#include "InventoryItemTooltip.generated.h"
+
+UCLASS()
+class SURVIVALGAMEITEMSRUNTIME_API UInventoryItemTooltip : public UCommonUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	
+	FORCEINLINE void SetItemData(const TSubclassOf<UItemDefinition> InItemDefinition, const int32 InItemCount)
+	{
+		ItemCount = InItemCount;
+		ItemDefinition = InItemDefinition;
+	}
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	TSubclassOf<UItemDefinition> ItemDefinition;
+	
+	UPROPERTY(BlueprintReadOnly)
+	int32 ItemCount;
+};
